08179a060a1a91c523f6ae9c0c6bbc73c2deb050
Cathal Duffy <cathalduffy@cathals-mbp.home>
2022-03-11 00:29:45 +0000

    Applied fix to API tests

--- test/api/user-api-test.js
+++ test/api/user-api-test.js
@@ -1,5 +1,5 @@
 import { assert } from "chai";
-import { playtimeService } from "./placemark-service.js";
+import { playtimeService } from "./placemark-service.js"; 
 import { assertSubset } from "../test-utils.js";
 import { maggie, testUsers } from "../fixtures.js";
 
